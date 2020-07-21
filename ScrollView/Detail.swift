7462f14396b64be78c2f4d12a6bdeda07c95b7e4
John Smith <app.mobile.368@gmail.com>
2020-07-21 13:58:08 -0700

    First

---
+++ ScrollView/Detail.swift
@@ -0,0 +1,14 @@
+//
+//  Detail.swift
+//  ScrollView
+//
+//  Created by NextUser on 7/21/20.
+//  Copyright © 2020 Swift&Xcode. All rights reserved.
+//
+
+import Foundation
+
+struct Person {
+    var name: String
+    
+}
